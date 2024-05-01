63c9d8fd25bfc8f69c79e3e1ab77a1f95463de30
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-05-01 14:38:38 +0700

    Transaction service implementation

--- src/grpc_server.rs
+++ src/grpc_server.rs
@@ -1,3 +1,5 @@
+use tokio::sync::mpsc::{self, Receiver, Sender};
+use tokio_stream::wrappers::ReceiverStream;
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod services {
@@ -6,8 +8,22 @@ pub mod services {
 
 use services::{
     payment_service_server::{PaymentService, PaymentServiceServer},
-    PaymentRequest, PaymentResponse,
+    transaction_service_server::{TransactionService, TransactionServiceServer},
+    PaymentRequest, PaymentResponse, TransactionRequest, TransactionResponse,
 };
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let payment_service = MyPaymentService::default();
+    let transaction_service = MyTransactionService::default();
+    Server::builder()
+        .add_service(PaymentServiceServer::new(payment_service))
+        .add_service(TransactionServiceServer::new(transaction_service))
+        .serve(addr)
+        .await?;
+    Ok(())
+}
 
 #[derive(Default)]
 pub struct MyPaymentService {}
@@ -23,14 +39,42 @@ impl PaymentService for MyPaymentService {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let payment_service = MyPaymentService::default();
+#[derive(Default)]
+pub struct MyTransactionService {}
 
-    Server::builder()
-        .add_service(PaymentServiceServer::new(payment_service))
-        .serve(addr)
-        .await?;
-    Ok(())
+#[tonic::async_trait]
+impl TransactionService for MyTransactionService {
+    type GetTransactionHistoryStream = ReceiverStream<Result<TransactionResponse, Status>>;
+
+    async fn get_transaction_history(
+        &self,
+        _request: Request<TransactionRequest>,
+    ) -> Result<Response<Self::GetTransactionHistoryStream>, Status> {
+        let (tx, rx): (
+            Sender<Result<TransactionResponse, Status>>,
+            Receiver<Result<TransactionResponse, Status>>,
+        ) = mpsc::channel(4);
+
+        tokio::spawn(async move {
+            for i in 0..30 {
+                if tx
+                    .send(Ok(TransactionResponse {
+                        transaction_id: format!("trans {}", i),
+                        status: String::from("Completed"),
+                        amount: 100.0,
+                        timestamp: String::from("2022-01-01T12:00:00Z"),
+                    }))
+                    .await
+                    .is_err()
+                {
+                    break;
+                }
+                if i % 10 == 9 {
+                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                }
+            }
+        });
+
+        Ok(Response::new(ReceiverStream::new(rx)))
+    }
 }
